3bf6f6f9f191ff4ff2b0f9d7bed2d25831175763
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-10-21 00:38:50 +0900

    DFS 코드 추가

---
+++ Algorithm/src/main/kotlin/dfs/DFS.kt
@@ -0,0 +1,29 @@
+package dfs
+
+fun main() {
+    val visited = BooleanArray(9)
+    val graph = Array(9) { ArrayList<Int>() }
+
+    fun dfs(x: Int) {
+        visited[x] = true
+        print("$x ")
+
+        for (i in 0..<graph[x].size) {
+            val y = graph[x][i]
+            if (visited[y].not()) {
+                dfs(y)
+            }
+        }
+    }
+
+    graph[1].add(2); graph[1].add(3); graph[1].add(8)
+    graph[2].add(1); graph[2].add(7)
+    graph[3].add(1); graph[3].add(4); graph[3].add(5)
+    graph[4].add(3); graph[4].add(5)
+    graph[5].add(3); graph[5].add(4)
+    graph[6].add(7)
+    graph[7].add(2); graph[7].add(6); graph[7].add(8)
+    graph[8].add(1); graph[8].add(7)
+
+    dfs(1)
+}
