ab47a8e0753d5259031a25d6c7b3832129442d5e
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-12-25 23:18:46 +0900

    [baekjoon] 2338 파일 추가

---
+++ baekjoon/src/main/kotlin/bronze/2338.kt
@@ -0,0 +1,12 @@
+package bronze
+
+import java.math.BigInteger
+
+fun main() {
+    val a = BigInteger(readln())
+    val b = BigInteger(readln())
+
+    println("${a + b}")
+    println("${a - b}")
+    println("${a * b}")
+}
