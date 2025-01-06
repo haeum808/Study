83a555bb7d89134929e7362e129fffa9df94c7d9
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-01-06 17:01:40 +0900

    [android-basic-with-compose] Happy Birthday 구현

--- codelab/android-basic-with-compose/Unit 1/Happy Birthday/app/build.gradle.kts
+++ codelab/android-basic-with-compose/Unit 1/Happy Birthday/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "org.haeum.happybirthday"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.haeum.happybirthday"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
