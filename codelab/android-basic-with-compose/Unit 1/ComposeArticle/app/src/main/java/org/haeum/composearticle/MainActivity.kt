37de4bc01fdb02a473dadc3adee785af6585fea6
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-01-06 18:09:36 +0900

    [android-basic-with-compose] Compose Article Composable 메서드들 위치 변경

--- codelab/android-basic-with-compose/Unit 1/ComposeArticle/app/src/main/java/org/haeum/composearticle/MainActivity.kt
+++ codelab/android-basic-with-compose/Unit 1/ComposeArticle/app/src/main/java/org/haeum/composearticle/MainActivity.kt
@@ -37,53 +37,53 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
-    @Composable
-    fun ComposeArticleApp() {
-        ArticleCard(
-            title = stringResource(R.string.title_jetpack_compose_tutorial),
-            shortDescription = stringResource(R.string.compose_short_desc),
-            longDescription = stringResource(R.string.compose_long_desc),
-            imagePainter = painterResource(R.drawable.bg_compose_background)
+@Composable
+fun ComposeArticleApp() {
+    ArticleCard(
+        title = stringResource(R.string.title_jetpack_compose_tutorial),
+        shortDescription = stringResource(R.string.compose_short_desc),
+        longDescription = stringResource(R.string.compose_long_desc),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+private fun ArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier) {
+        Image(
+            painter = imagePainter,
+            contentDescription = null,
+        )
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp,
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify,
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify,
         )
     }
+}
 
-    @Composable
-    private fun ArticleCard(
-        title: String,
-        shortDescription: String,
-        longDescription: String,
-        imagePainter: Painter,
-        modifier: Modifier = Modifier,
-    ) {
-        Column(modifier) {
-            Image(
-                painter = imagePainter,
-                contentDescription = null,
-            )
-            Text(
-                text = title,
-                modifier = Modifier.padding(16.dp),
-                fontSize = 24.sp,
-            )
-            Text(
-                text = shortDescription,
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-                textAlign = TextAlign.Justify,
-            )
-            Text(
-                text = longDescription,
-                modifier = Modifier.padding(16.dp),
-                textAlign = TextAlign.Justify,
-            )
-        }
-    }
-
-    @Preview(showBackground = true, showSystemUi = true)
-    @Composable
-    fun ComposeArticleAppPreview() {
-        ComposeArticleTheme {
-            ComposeArticleApp()
-        }
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ComposeArticleAppPreview() {
+    ComposeArticleTheme {
+        ComposeArticleApp()
     }
 }
