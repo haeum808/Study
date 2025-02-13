4518bdea657ead035b9636ee169a739a7b563cfe
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-02-13 18:57:42 +0900

    [스프링 입문] hello-spring 기초 세팅

---
+++ spring/hello-spring/src/test/kotlin/org/haeum/hello_spring/HelloSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.haeum.hello_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
