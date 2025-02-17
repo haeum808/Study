dff5ca6c78492801fd5957d9f5aae1ff2088d5fd
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-02-17 19:08:30 +0900

    [스프링 입문] MVC와 템플릿 엔진

--- spring/hello-spring/src/main/kotlin/org/haeum/hello_spring/controller/HelloController.kt
+++ spring/hello-spring/src/main/kotlin/org/haeum/hello_spring/controller/HelloController.kt
@@ -3,6 +3,7 @@ package org.haeum.hello_spring.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class HelloController {
@@ -11,5 +12,11 @@ class HelloController {
     fun hello(model: Model): String {
         model.addAttribute("data", "spring!!");
         return "hello"
+    }
+
+    @GetMapping("hello-mvc")
+    fun helloMvc(@RequestParam("name") name: String, model: Model): String {
+        model.addAttribute("name", name)
+        return "hello-template"
     }
 }
