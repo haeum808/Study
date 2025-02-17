8234944d924b4816b5139c2ad2fa9cb3524bfb58
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-02-17 21:00:28 +0900

    [스프링 입문] API

--- spring/hello-spring/src/main/kotlin/org/haeum/hello_spring/controller/HelloController.kt
+++ spring/hello-spring/src/main/kotlin/org/haeum/hello_spring/controller/HelloController.kt
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+
+class Hello(var name: String)
 
 @Controller
 class HelloController {
@@ -18,5 +21,18 @@ class HelloController {
     fun helloMvc(@RequestParam("name") name: String, model: Model): String {
         model.addAttribute("name", name)
         return "hello-template"
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    fun helloString(@RequestParam("name") name: String): String {
+        return "hello $name";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    fun helloApi(@RequestParam("name") name: String): Hello {
+        val hello = Hello(name)
+        return hello
     }
 }
