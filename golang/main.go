69854e17ee8cd5c04376f6415471e4355099fde8
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-05 15:26:26 +0900

    [쉽고 빠른 Go 시작하기] Packages and Imports

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"Study/golang/something"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("h")
+	something.SayHello()
+	// something.sayBye() -> 소문자 시작은 private 취
 }
