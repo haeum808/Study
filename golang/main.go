866f8b1e14437f3512947eeda109c2c2b2b0f2bf
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-05 15:33:24 +0900

    [쉽고 빠른 Go 시작하기] Variables and Constants

--- golang/main.go
+++ golang/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"Study/golang/something"
+	// "Study/golang/something"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("h")
-	something.SayHello()
-	// something.sayBye() -> 소문자 시작은 private 취
+	// something.SayHello()
+	// something.sayBye() -> 소문자 시작은 private 취급
+
+	const name string = "haeum"
+	// name = "haeum2" -> 상수라 변경 불가능
+	fmt.Println(name)
+	var name2 string = "haeum"
+	name2 = "haeum2"
+	fmt.Println(name2)
+	name3 := "haeum" // 타입 추론
+	name3 = "haeum2"
+	fmt.Println(name3)
 }
