c3d50e30a09333a1e7770ad352c8d412ce11d247
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-07 13:29:43 +0900

    [쉽고 빠른 Go 시작하기] Methods part Two

--- golang/bank/accounts/accounts.go
+++ golang/bank/accounts/accounts.go
@@ -1,12 +1,14 @@
 package accounts
 
-import "fmt"
+import "errors"
 
 // Account struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("can't withdraw")
 
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
@@ -15,12 +17,20 @@ func NewAccount(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
-	fmt.Println("Gonna deposit", amount)
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
 // Balance of your account
 func (a Account) Balance() int {
 	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- golang/bank/main.go
+++ golang/bank/main.go
@@ -9,4 +9,9 @@ func main() {
 	account := accounts.NewAccount("haeum")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
