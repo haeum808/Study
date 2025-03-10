7ed7668cbaa472d2ddd4c0af55f93ca0344cafe8
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-10 22:15:32 +0900

    [쉽고 빠른 Go 시작하기] extractJob part One

--- lecture/get-started-with-go-quickly-and-easily/job-scrapper/main.go
+++ lecture/get-started-with-go-quickly-and-easily/job-scrapper/main.go
@@ -8,19 +8,46 @@ import (
 	"strconv"
 )
 
+type extractedJob struct {
+	id           string
+	title        string
+	jobDate      string
+	jobCondition string
+	jobSector    string
+}
+
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
 func main() {
-	 totalPages := getPages()
+	totalPages := getPages()
 
-	 for i := 0; i < totalPages; i++ {
-		 getPage(i)
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
 	}
 }
 
 func getPage(page int) {
-	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page + 1)
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page+1)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("value")
+		title := card.Find(".area_job>h2").Text()
+		jobDate := card.Find(".job_date").Text()
+		jobCondition := card.Find(".job_condition").Text()
+		jobSector := card.Find(".job_sector").Text()
+		fmt.Println(id, title, jobDate, jobCondition, jobSector)
+	})
 }
 
 func getPages() int {
@@ -34,14 +61,14 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	doc.Find(".pagination").Each(func(i int,s *goquery.Selection) {
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
 		pages = s.Find("a").Length()
 	})
 
 	return pages
 }
 
-func checkErr(err error){
+func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
