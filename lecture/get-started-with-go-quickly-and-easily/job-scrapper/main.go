85ebc4e9ae6c2544d8801ccd08ed2ae2987a954d
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-10 14:27:40 +0900

    [쉽고 빠른 Go 시작하기] getPages part One

---
+++ lecture/get-started-with-go-quickly-and-easily/job-scrapper/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+)
+
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
+
+func main() {
+	getPages()
+}
+
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+
+	return 0
+}
+
+func checkErr(err error){
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status: ", res.StatusCode)
+	}
+}
