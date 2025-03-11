5be2bfd7f6ffed0c7b7a993ab50d88e4ed8627aa
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-11 22:06:35 +0900

    [Dart 시작하기] Lists

--- lecture/getting-started-with-dart/data types/main.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  // Basic Data Types
-  String name = 'haeum';
-  bool alive = true;
-  int age = 12;
-  double money = 69.99;
-  num x = 12;
-  x = 1.1;
-}
--- lecture/getting-started-with-dart/data-types/main.dart
+++ lecture/getting-started-with-dart/data-types/main.dart
@@ -1,1 +1,16 @@
-void main() {}
+void main() {
+  // Basic Data Types
+  String name = 'haeum';
+  bool alive = true;
+  int age = 12;
+  double money = 69.99;
+  num x = 12;
+  x = 1.1;
+
+  // Lists
+  var giveMeFive = true;
+  var numbers = [1, 2, 3, 4, if (giveMeFive) 5];
+  List<int> numbers2 = [1, 2, 3, 4];
+
+  print(numbers);
+}
