e619fe33e8a79b1dc235d1d4761db08577f30229
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-11 22:29:12 +0900

    [Dart 시작하기] String Interpolation

--- lecture/getting-started-with-dart/data-types/main.dart
+++ lecture/getting-started-with-dart/data-types/main.dart
@@ -13,4 +13,10 @@ void main() {
   List<int> numbers2 = [1, 2, 3, 4];
 
   print(numbers);
+
+  // String Interpolation
+  var name2 = 'haeum';
+  var age2 = 10;
+  var greeting = "Hello everyone, my name is $name2 and I'm ${age2 + 2}";
+  print(greeting);
 }
