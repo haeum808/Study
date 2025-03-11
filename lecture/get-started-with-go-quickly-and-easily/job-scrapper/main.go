df756aade4de8d74fae775960387318e9053d5e7
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-11 14:52:16 +0900

    [쉽고 빠른 Go 시작하기] File Download

--- lecture/get-started-with-go-quickly-and-easily/job-scrapper/main.go
+++ lecture/get-started-with-go-quickly-and-easily/job-scrapper/main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"github/scrapper"
+	"os"
 	"strings"
 )
+
+const fileName string = "jobs.csv"
 
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	return nil
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
