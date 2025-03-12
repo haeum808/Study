9a60385e779844a9a23b12486c88091acffc3915
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-12 17:31:48 +0900

    [Dart 시작하기] Constructors

---
+++ lecture/getting-started-with-dart/classes/main.dart
@@ -0,0 +1,15 @@
+// Your First Data Class
+class Player {
+  final String name = 'haeum';
+  int xp = 1500;
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
+void main() {
+  // Your First Data Class
+  var player = Player();
+  player.sayHello();
+}
