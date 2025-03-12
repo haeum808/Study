2812b9d7084d8265a39a686b5c1c5cfbfb0df253
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-12 12:06:07 +0900

    [Dart 시작하기] Named Parameters

--- lecture/getting-started-with-dart/functions/main.dart
+++ lecture/getting-started-with-dart/functions/main.dart
@@ -1,5 +1,10 @@
 void main() {
+  // Defining a Function
   sayHello('haeum');
+
+  // Named Parameters
+  sayHello2(); // default value
+  sayHello3(age: 19, country: 'cuba', name: 'haeum'); // required
 }
 
 // Defining a Function
@@ -8,3 +13,20 @@ String sayHello(String name) {
 }
 
 num plus(num a, num b) => a + b; // fat arrow syntax
+
+// Named Parameters
+String sayHello2({
+  String name = 'anon',
+  int age = 99,
+  String country = 'wakanda',
+}) {
+  return "Hello $name, you are $age, and you come from $country";
+}
+
+String sayHello3({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return "Hello $name, you are $age, and you come from $country";
+}
