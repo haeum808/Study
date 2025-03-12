2a01d55c6550ea98fc0295a949b9face7928ccc9
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-12 17:36:44 +0900

    [Dart 시작하기] Constructors

--- lecture/getting-started-with-dart/classes/main.dart
+++ lecture/getting-started-with-dart/classes/main.dart
@@ -8,8 +8,41 @@ class Player {
   }
 }
 
+// Constructors
+class Player2 {
+  late final String name;
+  late int xp;
+
+  Player2(String name, int xp) {
+    this.name = name;
+    this.xp = xp;
+  }
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
+class Player3 {
+  final String name;
+  int xp;
+
+  Player3(this.name, this.xp);
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
 void main() {
   // Your First Data Class
   var player = Player();
   player.sayHello();
+
+  // Constructors
+  var player2 = Player2('haeum', 1500);
+  player2.sayHello();
+
+  var player3 = Player3('haeum', 1500);
+  player3.sayHello();
 }
