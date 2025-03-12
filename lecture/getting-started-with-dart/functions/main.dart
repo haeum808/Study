d3acc76972c3747ec6cac0a89efbbf6e613ee1f8
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-12 11:54:40 +0900

    [Dart 시작하기] Defining a Function

---
+++ lecture/getting-started-with-dart/functions/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  sayHello('haeum');
+}
+
+// Defining a Function
+String sayHello(String name) {
+  return "Hello $name nice to meet you!";
+}
+
+num plus(num a, num b) => a + b; // fat arrow syntax
