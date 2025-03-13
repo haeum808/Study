48014b0421cb7b7c824b6b9f60ba394486eeb54f
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-13 15:21:31 +0900

    [Flutter로 웹툰 앱 만들기] Buttons Section

--- lecture/creating-a-webtoon-app-with-flutter/ui_chanllenge/lib/main.dart
+++ lecture/creating-a-webtoon-app-with-flutter/ui_chanllenge/lib/main.dart
@@ -21,6 +21,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 100),
               Row(
@@ -45,6 +46,41 @@ class App extends StatelessWidget {
                         ),
                       ),
                     ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 120),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  color: Colors.white.withValues(alpha: 0.8),
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text('Transfer', style: TextStyle(fontSize: 20)),
+                    ),
                   ),
                 ],
               ),
