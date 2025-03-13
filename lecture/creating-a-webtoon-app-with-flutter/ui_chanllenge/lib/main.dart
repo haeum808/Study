b392eecb51ab52683b706d72fcdb18af730eac05
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-13 14:35:10 +0900

    [Flutter로 웹툰 앱 만들기] Header

---
+++ lecture/creating-a-webtoon-app-with-flutter/ui_chanllenge/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Player {
+  String name;
+
+  Player({required this.name});
+}
+
+void main() {
+  var haeum = Player(name: 'haeum');
+  haeum.name;
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(height: 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withValues(alpha: 0.5),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
