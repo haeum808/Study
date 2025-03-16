8fae8eea0535ca7285f90992fe6fe35e6a431e1b
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-17 00:42:51 +0900

    [Flutter로 웹툰 앱 만들기] Recap

--- lecture/creating-a-webtoon-app-with-flutter/webtoon_app/lib/main.dart
+++ lecture/creating-a-webtoon-app-with-flutter/webtoon_app/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon_app/screens/home_screen.dart';
-import 'package:webtoon_app/services/ApiService.dart';
+import 'package:webtoon_app/services/api_service.dart';
 
 void main() {
-  Apiservice().getTodaysToons();
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
--- lecture/creating-a-webtoon-app-with-flutter/webtoon_app/lib/services/api_service.dart
+++ lecture/creating-a-webtoon-app-with-flutter/webtoon_app/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:webtoon_app/models/webtoon_model.dart';
 
-class Apiservice {
+class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
