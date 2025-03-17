a53ff21d353e8b6ae238aab94704d789742b8ba7
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-18 00:18:11 +0900

    [Flutter로 웹툰 앱 만들기] Webtoon Card

--- lecture/creating-a-webtoon-app-with-flutter/webtoon_app/lib/screens/home_screen.dart
+++ lecture/creating-a-webtoon-app-with-flutter/webtoon_app/lib/screens/home_screen.dart
@@ -21,19 +21,55 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => SizedBox(width: 20),
+            return Column(
+              children: [
+                SizedBox(height: 50),
+                Expanded(child: makeList(snapshot)),
+              ],
             );
           }
           return const Center(child: CircularProgressIndicator());
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: Offset(10, 10),
+                    color: Colors.black.withValues(alpha: 0.5),
+                  ),
+                ],
+              ),
+              width: 250,
+              child: Image.network(
+                headers: {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+                webtoon.thumb,
+              ),
+            ),
+            SizedBox(width: 10),
+            Text(webtoon.title, style: TextStyle(fontSize: 22)),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(width: 40),
     );
   }
 }
