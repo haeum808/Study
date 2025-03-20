129936d04e80f821b0a6547c8c053e6e39f9af1f
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-20 17:51:41 +0900

    [바닐라 JS로 크롬 앱 만들기] Returns

--- lecture/building-chrome-apps-with-vanilla-js/momentum/app.js
+++ lecture/building-chrome-apps-with-vanilla-js/momentum/app.js
@@ -80,3 +80,28 @@ const player2 = {
 
 console.log(player2.name);
 player2.sayHello("haeum2");
+
+// Returns
+const calculator = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+    times: function (a, b) {
+        return a * b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    },
+    power: function (a, b) {
+        return a ** b;
+    },
+};
+
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divid(timesResult, plusResult);
+const powerResult = calculator.power(divide, minusResult);
