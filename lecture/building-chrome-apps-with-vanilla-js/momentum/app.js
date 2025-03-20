e15c21cd44217cefe5ad7f0c351a8b253c43aa8a
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-20 10:54:51 +0900

    [바닐라 JS로 크롬 앱 만들기] const and let

--- lecture/building-chrome-apps-with-vanilla-js/momentum/app.js
+++ lecture/building-chrome-apps-with-vanilla-js/momentum/app.js
@@ -7,3 +7,16 @@ console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+// const and let
+const a2 = 5;
+const b2 = 2;
+let myName2 = "haeum";
+
+console.log(a2 + b2);
+console.log(a2 * b2);
+console.log(a2 / b2);
+console.log("hello " + myName2);
+
+myName2 = "haeum808";
+console.log("your new name is " + myName2);
