f48c59fa42fb24fb189062e3c56258a5b3c1e1e6
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-17 11:18:14 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 150ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250417/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val (n1, n2) = readLine()!!.split(" ").map { it.toInt() }
+    val a = readLine()!!.split(" ").map { it.toInt() }
+    val b = readLine()!!.split(" ").map { it.toInt() }
+
+    if (isContinuousSubsequence(a, b)) {
+        println("Yes")
+    } else {
+        println("No")
+    }
+}
+
+fun isContinuousSubsequence(a: List<Int>, b: List<Int>): Boolean {
+    if (a.size < b.size) return false
+
+    for (i in 0..(a.size - b.size)) {
+        if (isAllSame(a.slice(i until i + b.size), b)) return true
+    }
+
+    return false
+}
+
+fun isAllSame(a: List<Int>, b: List<Int>): Boolean {
+    return a == b
+}
