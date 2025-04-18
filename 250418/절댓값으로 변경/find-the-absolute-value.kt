f6fac91e26c0519a3c0fb67fd45bc07a91f6487a
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-18 17:36:08 +0900

    Title: 절댓값으로 변경, Time: 150ms, Memory: 15MB, Status: Passed - Codetree

--- 250418/절댓값으로 변경/find-the-absolute-value.kt
+++ 250418/절댓값으로 변경/find-the-absolute-value.kt
@@ -2,7 +2,7 @@ import kotlin.math.abs
 
 fun main() {
     val n = readLine()!!.toInt()
-    val numbers = readln().split(" ").map { it.toInt() }
+    val numbers = readln().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
     
     println(numbers.map { abs(it) }.joinToString(" "))
 }
