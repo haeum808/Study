2f5a318b0e4ab57a42d34e23d5f901b485331b8e
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-18 11:36:41 +0900

    Title: 짝수만 2로 나누기, Time: 135ms, Memory: 12MB, Status: Passed - Codetree

--- 250418/짝수만 2로 나누기/divide-even-numbers-by-2.kt
+++ 250418/짝수만 2로 나누기/divide-even-numbers-by-2.kt
@@ -1,6 +1,6 @@
 fun main() {
     val n = readLine()!!.toInt()
-    val arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+    val arr = readLine()!!.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toMutableList()
 
     for (i in arr.indices) {
         if (arr[i] % 2 == 0) {
