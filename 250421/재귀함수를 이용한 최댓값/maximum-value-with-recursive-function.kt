1e7f58a0736f40814f4c410bbf923520dedb9220
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-21 13:05:24 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 157ms, Memory: 12MB, Status: Passed - Codetree

--- 250421/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.kt
+++ 250421/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.kt
@@ -2,11 +2,10 @@ fun main() {
     val n = readLine()!!.toInt()
     val arr = readln().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
 
-    println(findMax(arr, 0))
+    println(findMax(arr, 0, 0))
 }
 
-fun findMax(arr : List<Int>, max: Int): Int {
-    if (arr.size == 0) return max
-
-    return findMax(arr.slice(1 until arr.size), maxOf(max, arr[0]))
+fun findMax(arr: List<Int>, index: Int, currentMax: Int): Int {
+    if (index == arr.size) return currentMax
+    return findMax(arr, index + 1, maxOf(currentMax, arr[index]))
 }
