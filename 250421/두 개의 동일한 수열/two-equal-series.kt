4b56df117ec1b376cde511eeae727157ec4dd250
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-21 23:44:55 +0900

    Title: 두 개의 동일한 수열, Time: 181ms, Memory: 17MB, Status: Passed - Codetree

--- 250421/두 개의 동일한 수열/two-equal-series.kt
+++ 250421/두 개의 동일한 수열/two-equal-series.kt
@@ -1,7 +1,7 @@
 fun main() {
     val n = readLine()!!.toInt()
-    val a = readLine()!!.split(" ").map { it.toInt() }.sorted()
-    val b = readLine()!!.split(" ").map { it.toInt() }.sorted()
+    val a = readLine()!!.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.sorted()
+    val b = readLine()!!.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.sorted()
 
     if (a == b) {
         println("Yes")
