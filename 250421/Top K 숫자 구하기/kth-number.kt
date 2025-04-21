22ee981c07eb151fe33484f760443e34749bb03e
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-21 23:41:33 +0900

    Title: Top K 숫자 구하기, Time: 209ms, Memory: 19MB, Status: Passed - Codetree

--- 250421/Top K 숫자 구하기/kth-number.kt
+++ 250421/Top K 숫자 구하기/kth-number.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val (n, k) = readLine()!!.split(" ").map { it.toInt() }
-    val nums = readLine()!!.split(" ").map { it.toInt() }
+    val (n, k) = readLine()!!.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+    val nums = readLine()!!.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
     println(nums.sorted()[k - 1])
 }
