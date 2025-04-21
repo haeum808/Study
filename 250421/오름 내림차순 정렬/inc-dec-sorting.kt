3e3ed6983611fbee6c1b621282582c6883ff1e4f
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-21 23:26:37 +0900

    Title: 오름 내림차순 정렬, Time: 184ms, Memory: 18MB, Status: Passed - Codetree

--- 250421/오름 내림차순 정렬/inc-dec-sorting.kt
+++ 250421/오름 내림차순 정렬/inc-dec-sorting.kt
@@ -1,6 +1,6 @@
 fun main() {
     val n = readLine()!!.toInt()
-    val nums = readLine()!!.split(" ").map { it.toInt() }
+    val nums = readLine()!!.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
 
     println(nums.sorted().joinToString(" "))
     println(nums.sortedDescending().joinToString(" "))
