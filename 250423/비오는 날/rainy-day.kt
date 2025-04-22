13878245410a4b8e048bf07dc7afcb08aecb506f
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-23 01:43:03 +0900

    Title: 비오는 날, Time: 152ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 250423/비오는 날/rainy-day.kt
@@ -0,0 +1,42 @@
+class WeatherForecast(
+    val year: String,
+    val month: String,
+    val day: String,
+    val dayOfTheWeek: String,
+    val weather: String,
+) {
+    override fun toString(): String {
+        return "$year-$month-$day $dayOfTheWeek $weather"
+    }
+}
+
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val forecasts = mutableListOf<Triple<String, String, String>>()
+    for (i in 1..n) {
+        val (date, day, weather) = readln().split(" ")
+        forecasts.add(Triple(date, day, weather))
+    }
+
+    val weatherForecasts = forecasts.map { 
+        val date = it.first.split("-")
+        WeatherForecast(date[0], date[1], date[2], it.second, it.third) 
+    }
+    var result: WeatherForecast? = null
+
+    for (weatherForecast in weatherForecasts) {
+        if (result == null && weatherForecast.weather == "Rain") {
+            result = weatherForecast
+        } else if (result != null && 
+            weatherForecast.weather == "Rain" &&
+            weatherForecast.year.toInt() < result.year.toInt() &&
+            weatherForecast.month.toInt() < result.month.toInt() &&
+            weatherForecast.day.toInt() < result.day.toInt()
+        ) {
+            result = weatherForecast
+        }
+    }
+
+    println(result)
+}
