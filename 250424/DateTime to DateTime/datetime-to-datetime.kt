e50ba6ee587ecbac89d3432e2a9b7b82c14b7301
Joon Kyung Lee <haeumueah@gmail.com>
2025-04-25 16:19:28 +0900

    Title: DateTime to DateTime, Time: 114ms, Memory: 12MB, Status: Wrong Answer - Codetree

--- 250424/DateTime to DateTime/datetime-to-datetime.kt
+++ 250424/DateTime to DateTime/datetime-to-datetime.kt
@@ -1,9 +1,9 @@
 fun main() {
     val (a, b, c) = readln().split(" ").map { it.toInt() }
 
-    if (a == 11 && b < 11) {
+    if (b < 11) {
         println(-1)
-    } else if (a == 11 && b < 11 && c < 11) {
+    } else if (b == 11 && c < 11) {
         println(-1)
     } else {
         val result1 = 11 * 1440 + 11 * 60 + 11
