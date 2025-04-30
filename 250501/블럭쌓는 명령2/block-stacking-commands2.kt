aafc8ad7db2e429978f4544499949ee79ff773b6
Joon Kyung Lee <haeumueah@gmail.com>
2025-05-01 01:39:36 +0900

    Title: 블럭쌓는 명령2, Time: 153ms, Memory: 16MB, Status: Passed - Codetree

--- 250501/블럭쌓는 명령2/block-stacking-commands2.kt
+++ 250501/블럭쌓는 명령2/block-stacking-commands2.kt
@@ -1,13 +1,13 @@
 fun main() {
     val (n, k) = readln().split(" ").map { it.toInt() }
-    val a = IntArray(n + 1)
-    val b = IntArray(n + 1)
+    val a = IntArray(k)
+    val b = IntArray(k)
     for (i in 0 until k) {
         val values = readln().split(" ").map { it.toInt() }
         a[i] = values[0]
         b[i] = values[1]
     }
-    var result = IntArray(n + 1)
+    val result = IntArray(n + 1)
 
     for (i in 0 until k) {
         for (j in a[i]..b[i]) {
